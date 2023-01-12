a3c3a5f833efdfc91b3a90520791a009673d2a77
IlMaxone <maxo.paxo43@gmail.com>
2023-01-12 16:08:02 +0100

    aggiornamento

---
+++ files/DB base/cafmi.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cafmi
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ruolo`
+--
+
+DROP TABLE IF EXISTS `ruolo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ruolo` (
+  `id_ruolo` int NOT NULL,
+  `posizione` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_ruolo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ruolo`
+--
+
+LOCK TABLES `ruolo` WRITE;
+/*!40000 ALTER TABLE `ruolo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ruolo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `codice_cliente` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `cognome` varchar(45) NOT NULL,
+  `numero_telefono` int NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `indirizzo` varchar(45) NOT NULL,
+  `citta` varchar(45) NOT NULL,
+  `cap_citta` int NOT NULL,
+  `codice_fiscale` varchar(45) NOT NULL,
+  `sesso` char(1) NOT NULL,
+  `cod_ruolo` int NOT NULL,
+  PRIMARY KEY (`codice_cliente`),
+  UNIQUE KEY `codice_fiscale_UNIQUE` (`codice_fiscale`),
+  KEY `id_ruolo_idx` (`cod_ruolo`),
+  CONSTRAINT `id_ruolo` FOREIGN KEY (`cod_ruolo`) REFERENCES `ruolo` (`id_ruolo`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-21 14:40:58
---
+++ files/JSON/Readme.txt
@@ -0,0 +1,7 @@
+Queste cartelle servono a far funzionare il JSON della webapp CAFMi
+Per poter sfruttare al 100% JSON fai questi passaggi:
+- apri esplora risorse e vai su C:\
+- estrai il contenuto di questo zip in C:\
+- assicurati di avere la cartella JSON in C:\
+ora il gioco è fatto e hai integrato tutta le cartelle necessarie al linguaggio JSON per funzionare,
+i rispettivi file si creeranno da soli nelle cartelle, puoi scegliere se tenerti le liste di base già qui dento oppure cancellare tutti i file
